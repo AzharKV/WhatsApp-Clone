3609e0f419aa8bbcc6e82b8c844c9af652cd4c1c
Azhar <mohammedazharkv@gmail.com>
2022-02-12 20:30:34 +0530

    package changes

--- frontend_flutter/lib/services/api_names.dart
+++ frontend_flutter/lib/services/api_names.dart
@@ -2,4 +2,7 @@ import 'package:whatsapp_clone/const_files/keys/server_keys.dart';
 
 class Api {
   static const String myDetails = ServerKeys.baseurl + "myDetails";
+  static const String usersList = ServerKeys.baseurl + "getUsers";
+  static const String userStatus = ServerKeys.baseurl + "userStatus/";
+  static const String sendMessage = ServerKeys.baseurl + "sendMessage";
 }
